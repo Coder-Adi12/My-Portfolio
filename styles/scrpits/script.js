1c11b414cf5c78eef2c8bd1a9ef61ed2da852343
Aditya Borhade <adityaborhade62@gmail.com>
2024-08-24 23:46:46 -0600

    Initial commit

---
+++ styles/scrpits/script.js
@@ -0,0 +1,6 @@
+// This script is a placeholder for any JavaScript functionality you might add to your portfolio in the future
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Portfolio site is ready!');
+    // Future JS functionality can go here
+});
